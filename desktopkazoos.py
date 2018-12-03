d34d11dc1dfb172bba1d94cf50493a42e5560b82
Jiffy953 <44827562+Jiffy953@users.noreply.github.com>
2018-12-02 20:27:17 -0500

    yeet new satuff

--- desktopkazoos.py
+++ desktopkazoos.py
@@ -1,7 +1,7 @@
 import random
 import os
-login = os.getlogin()
 
+login = os.getlogin()
 num = input("How may kazoos would you like to add to your desktop? ")
 for x in range(int(num)):
     f = open(f"C:\\Users\\{login}\\Desktop\\brandnewkazoo{random.randint(0, 100)}.txt", "w+")
@@ -17,4 +17,27 @@ for x in range(int(num)):
 88   `Y8a `"8bbdP"Y8 888888888  `"YbbdP"'   `"YbbdP"'
 \r\n''')
     f.close()
-print("Enjoy!")
+
+def kazoo_noise_number():
+    if int(num) == 1:
+        os.startfile('C:\\Users\\Jared\\Documents\\GitHub\\Programs\\Recording')
+    elif int(num) == 2:
+        os.startfile('C:\\Users\\Jared\\Documents\\GitHub\\Programs\\Merged0_1.mp3')
+    elif int(num) == 3:
+        os.startfile('C:\\Users\\Jared\\Documents\\GitHub\\Programs\\Merged0_2.mp3')
+    elif int(num) == 4:
+        os.startfile('C:\\Users\\Jared\\Documents\\GitHub\\Programs\\Merged0_3.mp3')
+    elif int(num) == 4:
+        os.startfile('C:\\Users\\Jared\\Documents\\GitHub\\Programs\\Merged0_4.mp3')
+    elif int(num) == 5:
+        os.startfile('C:\\Users\\Jared\\Documents\\GitHub\\Programs\\Merged0_5.mp3')
+    elif int(num) == 6:
+        os.startfile('C:\\Users\\Jared\\Documents\\GitHub\\Programs\\Merged0_6.mp3')
+    elif int(num) == 7:
+        os.startfile('C:\\Users\\Jared\\Documents\\GitHub\\Programs\\Merged0_7.mp3')
+    elif int(num) == 8:
+        os.startfile('C:\Users\Jared\Documents\GitHub\Programs\Merged0_8.mp3')
+    else:
+        os.startfile('C:\Users\Jared\Documents\GitHub\Programs\Merged0_9.mp3')
+kazoo_noise_number()
+input("Enjoy!")
